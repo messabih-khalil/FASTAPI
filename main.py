30a3f087ae6d9b6e8cdd9c3da7446a271e6a42a1
messabih-khalil <msbih.khalil@gmail.com>
2022-10-30 06:42:16 +0100

    request body

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
+class Candy(BaseModel):
+    name : str
+    recipe: str
+    piece : Optional[bool] = False
 
 # init app
 
@@ -10,21 +15,11 @@ app = FastAPI()
 
 # start api #
 
-@app.get('/comments')
+@app.post('/candy')
 
-def comments(limit : int = 10 , date : str = '2022-12-12' , order : Optional[str] = None):
-    
-    return{
-        "comments" : [
-            {
-                '1' : 'h',
-                '2' : 'e',
-                '3' : 'l',
-                '4' : 'l',
-                '5' : 'o',
-            },
-        ],
-
-        'limit' : limit,
-        'date' : date
+def candy(request : Candy):
+    return {
+        'name' : request.name,
+        'recipe' : request.recipe,
+        'is_piece' : request.piece
     }

4c6dd6669b5da11ab2403edd58810540a3fd7fa4
messabih-khalil <msbih.khalil@gmail.com>
2022-11-06 06:18:44 +0100

    update

--- main.py
+++ main.py
@@ -1,77 +1,28 @@
-from typing import List
-from fastapi import Depends, FastAPI, HTTPException , status
+from fastapi import FastAPI , Query , Path
 
-from sqlalchemy.orm import Session
 
-from models import models
-from schemas import schemas
-
-from database import SessionLocal, engine
+# app
 
 app = FastAPI()
 
+# query params with validation
 
-models.Base.metadata.create_all(bind = engine)
-
-def db():
-    db = SessionLocal()
-
-    try:
-        yield db
+###
+# str | none
+# length with Query class
+###
+@app.get('/products/')
+def products(name : str | None = Query(default=None, max_length=50)):
     
-    finally:
-        db.close()
-
-
-@app.get('/')
-def home():
     return {
-        "message" : "hello world"
+        'product_name' : name
     }
 
-# * Create user 
-@app.post('/user')
-def createUser(request : schemas.CreateUserSchema , db : Session = Depends(db) , status_code=status.HTTP_201_CREATED):
-    user = models.User(id = request.id , username = request.username , password = request.password)
-    
-    # insert new user to db
 
-    db.add(user)
-    db.commit()
-    db.refresh(user)
+# path-params-numeric-validations
 
-    return user
-
-# * get new user
-
-@app.get('/user' , response_model=List[schemas.UserResponse])
-def getUser(db : Session = Depends(db)):
-    user = db.query(models.User).all()
-    
-    return user
-
-# * get user by id
-
-@app.get('/user/{id}' , status_code=status.HTTP_200_OK)
-def getMyUser(id : int , db : Session = Depends(db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if user:
-        return user
-
-    raise HTTPException(detail='user not exist' , status_code=status.HTTP_404_NOT_FOUND)
-
-
-# * delete user 
-
-@app.delete('/user/{id}')
-def deleteUser(id : int ,db : Session = Depends(db)):
-    user = db.query(models.User).filter(models.User.id == id)
-
-    if user :
-        user.delete(synchronize_session=False)
-        db.commit()
-        return {
-            "message" : "deleted"
-        }
-
-    raise HTTPException(detail='user not exist' , status_code=status.HTTP_404_NOT_FOUND)
+@app.get('/users/{id}')
+def user(id : int = Path(tile="THe id of the user")):
+    return {
+        "user_id" : id
+    }

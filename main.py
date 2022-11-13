aa9c5c28c151d73878f7abeba07a31cbda6da4f1
messabih-khalil <msbih.khalil@gmail.com>
2022-11-13 07:51:11 +0100

    extra models

--- main.py
+++ main.py
@@ -5,77 +5,40 @@ from pydantic import BaseModel , Field
 
 app =FastAPI()
 
-fake_users = [
-    {
-        "id" : 1,
-        "username" : "tars",
-        "email" : "tars@gmail.com",
-        "password" : "123456789"
-    },
-    {
-        "id" : 2,
-        "username" : "aldin",
-        "email" : "aldin@gmail.com",
-        "password" : "123456789"
-    }
-]
-
-
-class User(BaseModel):
-    # id : int
+class BaseUser(BaseModel):
     username : str
     email : str
 
-class CreateUser(BaseModel):
-    username : str
-    email : str
+class UserResponse(BaseUser):
+    id : int
+
+class UserRequest(BaseUser):
+    id : int
     password : str
 
-# add user
-@app.post("/user" , response_model=User)
-def addUser(request : CreateUser):
-    
-    new_user = {
-        "id" : 3,
-        "username" : request.username,
-        "email" : request.email,
-        "password" : request.password
-    }
-    fake_users.append(new_user)
-
-    return new_user
+class UserInDB(BaseUser):
+    id : int
+    hashed_password : str 
 
 
-# get users
-@app.get("/user" , response_model=list[User])
-def allUsers():
-    return fake_users
-# get user
-@app.get('/user/{id}' , response_model=User)
-def getUser(id : int | None):
-    user = [a for a in fake_users if a["id"] in id]
-    print(user)
-    return user
+fake_user = []
 
-# update User
-
-# delete user
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float = 10.5
-    tags: list[str] = []
+# hash password
+def hashPassword(password : str):
+    return f"hashed:##@${password}"
 
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
+# save to db
+def saveToDb(user : UserRequest):
+    hashed_password = hashPassword(user.password)
+    fake_new_user = UserInDB(**user.dict() , hashed_password = hashed_password)
+  
+    return fake_new_user
 
+# add new user 
 
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: str):
-    return items[item_id]
+@app.post('/user' , response_model=UserInDB)
+def createUser(request : UserRequest):
+    fake_user = saveToDb(request)
+
+    return fake_user
